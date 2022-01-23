efaa7ac29a0565e7698d5f5f993ba21836a7feb2
KratosvsEzio <jadude19@gmail.com>
2022-01-24 00:54:09 +0500

    secrects removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  rapidAPIKey: 'f5f702d37amsh1b1c4b135a92f04p184bf6jsn8dc906a0ce6a',
+  rapidAPIKey: '',
   wordLookUpBaseUrl: 'https://wordsapiv1.p.rapidapi.com/words'
 };
 
